15440d51a4dd94834b7b924f8931bb9f6349f469
Kevin Ventocilla <kevinventocilla7@gmail.com>
2026-06-04 15:24:37 -0500

    Montar rutas de reportes en el servidor

--- server.js
+++ server.js
@@ -150,6 +150,7 @@ app.use('/api', require('./routes/pedidos.routes'));
 app.use('/api', require('./routes/caja.routes'));
 app.use('/api', require('./routes/eventos.routes'));
 app.use('/api/usuarios', require('./routes/usuarios.routes'));
+app.use('/api', require('./routes/reportes.routes')); // NUEVA LÍNEA AÑADIDA
 
 // ==========================================
 // 5. MANEJO DE ERRORES CENTRALIZADO
